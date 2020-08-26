a21a0779c5ffa8801edbe2625348c532ff0dc1ad
dennis <dennis.olimpio0794@gmail.com>
2020-08-26 17:16:33 +0200

    ho inserito la classe user in un file a parte. Aggiunti attributi e ista
    user.php

---
+++ User.php
@@ -0,0 +1,15 @@
+<?php
+// creo la mia classe
+class User {
+  // definisco le variabili/ attributi
+  public $nome;
+  public $cognome;
+  public $provenienza;
+  public $eta;
+  public $email;
+  public $descrizione;
+
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,50 @@
 <?php
 
+require_once(__DIR__ . "/User.php");
 
- ?>
+$users = [
+  [
+    'id' => 1,
+    'nome' => 'Dennis',
+    'cognome' => 'Olimpio ',
+    'provenienza' => 'Napoli',
+    'eta' => 26,
+    'email' => 'dennis@test.it',
+    'descrizione' => 'studenti boolean, classe 14'
+  ],
+  [
+    'id' => 2,
+    'nome' => 'Jessica',
+    'cognome' => 'piazza ',
+    'provenienza' => 'Lecce',
+    'eta' => 31,
+    'email' => 'jessica@test.it',
+    'descrizione' => ' Lorem ipsum dolor sit amet'
+  ],
+  [
+    'id' => 3,
+    'nome' => 'Sveva',
+    'cognome' => 'Ferrari ',
+    'provenienza' => 'Roma',
+    'eta' => 77,
+    'email' => 'sveva@test.it',
+    'descrizione' => 'Lorem ipsum dolor sit amet'
+  ],
+  [
+    'id' => 4,
+    'nome' => 'Timoteo',
+    'cognome' => 'Ferrari ',
+    'provenienza' => 'Formia',
+    'eta' => 23,
+    'email' => 'teoteo@test.it',
+    'descrizione' => 'Lorem ipsum dolor sit amet'
+  ],
+
+];
+
+var_dump($users)
+
+// $utente = new users
+
+
+?>
